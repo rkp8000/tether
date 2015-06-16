0c5cba01f887da0c426135e16286e6187102149e
rkp8000 <rpang.contact@gmail.com>
2015-06-16 10:42:10 -0700

    Fix import bug.

--- db_api/models.py
+++ db_api/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, ForeignKey
-from sqlalchemy import Integer, String, DateTime
+from sqlalchemy import Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
