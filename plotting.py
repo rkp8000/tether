fdf8dd22db78e31db438a76627ce3dd6efc17b4a
rkp8000 <rpang.contact@gmail.com>
2015-06-17 15:48:42 -0700

    Add function to return idxs of segments and to properly sort them.

--- plotting.py
+++ plotting.py
@@ -40,7 +40,7 @@ class SegmentSelector(object):
         self.segment_color = 'r'
 
     def __call__(self, event):
-        print('click', event)
+
         if event.button == 3:
             # get clicked location and force it into bounds
             t = event.xdata
@@ -55,20 +55,49 @@ class SegmentSelector(object):
                 for ax in self.axs:
                     ax.axvline(t, color='k', lw=1)
             else:
+                # store segment
+                segment = (self.start_t, t)
+                if self.start_t > t:
+                    segment = segment[::-1]
+                self.segments += [segment]
+
+                self.segments = sorted(self.segments, key=lambda seg: seg[0])
+
                 # fill in a rectangle between the line at the start location and the line at the
                 # clicked location
                 for ax in self.axs:
                     ax.axvline(t, color='k', lw=1)
                     ax.axvspan(self.start_t, t, alpha=0.3, color=self.segment_color)
-                    segment = (self.start_t, t)
-                    if self.start_t > t:
-                        segment = segment[::-1]
-                self.segments += [segment]
+
             self.start = not self.start
+
         plt.draw()
 
     def show_segments(self):
         print('TO DO: make this function show the segments on the axes')
+
+    def idx_from_time(self, t):
+        """
+        Use self.time_vector to compute the index of the time in time vector that is
+        nearest to t.
+        :param t: floating point time
+        :return:
+        """
+        if self.time_vector is None:
+            raise TypeError('time_vector is not defined')
+
+        dt = self.time_vector[1] - self.time_vector[0]
+        idx = int(round(t / dt))
+        if idx < 0:
+            idx = 0
+        elif idx > len(self.time_vector) - 1:
+            idx = len(self.time_vector) - 1
+
+        return idx
+
+    @property
+    def segment_idxs(self):
+        return [(self.idx_from_time(s[0]), self.idx_from_time(s[1])) for s in self.segments]
 
 
 def plot_trial_basic(trial, columns=None):
--- test_plotting.py
+++ test_plotting.py
@@ -2,6 +2,7 @@ from __future__ import division, print_function
 
 import unittest
 
+import pprint as pp
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -19,7 +20,7 @@ class SegmentSelectorTestCase(unittest.TestCase):
     def test_correct_number_of_segments_stored(self):
         fig, axs = plt.subplots(2, 1, sharex=True)
 
-        t = np.linspace(0, 100, 1000)
+        t = np.linspace(0, 100, 1001)
         x0 = np.random.normal(0, 1, t.shape)
         x1 = np.random.normal(0, 1, t.shape)
 
@@ -30,8 +31,24 @@ class SegmentSelectorTestCase(unittest.TestCase):
 
         plt.show()
 
-        print(segment_selector.segments)
+        pp.pprint(segment_selector.segments)
+        pp.pprint(segment_selector.segment_idxs)
+
         self.assertTrue(True)
+
+    def test_idx_from_time(self):
+        fig, axs = plt.subplots(3, 1, sharex=True)
+
+        t = np.linspace(0, 100, 1001)
+        segment_selector = plotting.SegmentSelector(fig, axs, time_vector=t)
+
+        self.assertEqual(segment_selector.idx_from_time(.3), 3)
+        self.assertEqual(segment_selector.idx_from_time(.29), 3)
+        self.assertEqual(segment_selector.idx_from_time(.24), 2)
+        self.assertEqual(segment_selector.idx_from_time(50.1), 501)
+        self.assertEqual(segment_selector.idx_from_time(50.14), 501)
+        self.assertEqual(segment_selector.idx_from_time(-1), 0)
+        self.assertEqual(segment_selector.idx_from_time(10000), 1000)
 
 
 if __name__ == '__main__':
