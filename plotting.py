d31cfd22f4d7a4c9e4325d457cda12d8f870a6dd
Rich Pang <rpang.contact@gmail.com>
2015-06-17 11:11:46 -0700

    Just to have a checkpoint.

--- plotting.py
+++ plotting.py
@@ -1,30 +1,41 @@
 from __future__ import print_function, division
 
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+import edr_handling
 
-class VerticalLineAdder(object):
-    def __init__(self, fig, axs):
+ARENA_DATA_DIRECTORY = os.getenv('ARENA_DATA_DIRECTORY')
+
+
+class SegmentSelector(object):
+    def __init__(self, fig, axs, segments=None):
         self.fig = fig
         self.axs = axs
         self.xs = [0]
         self.cid = fig.canvas.mpl_connect('button_press_event', self)
 
+        if segments is None:
+            self.segments = []
+        else:
+            self.segments = segments
+
+        # indicator determining whether next click will be start or end of segment
+        self.start = True
+
     def __call__(self, event):
         print('click', event)
         x = event.xdata
         if event.button == 3:
-            for ax in axs:
+            for ax in self.axs:
                 ax.vlines(x, -3, 3, color='k', lw=3)
         plt.draw()
 
-if __name__ == '__main__':
-    fig, axs = plt.subplots(3, 1, sharex=True, sharey=True)
-    for ax in axs:
-        ax.plot(np.random.normal(0, 1, 1000,))
-        ax.set_ylim(-3, 3)
 
-    VerticalLineAdder(fig, axs)
+def plot_trial_basic(trial, columns=None):
+    """Plot the edr file corresponding to a trial."""
+    experiment_directory_path = os.path.join(ARENA_DATA_DIRECTORY, trial.experiment.directory_path)
+    file_path = os.path.join(experiment_directory_path, trial.file_name)
 
-    plt.show()
+    data = edr_handling.load_edr(file_path)[0]
