1f0f226f6ed0a2e45145c095c0e03ede57e745fd
rkp8000 <rpang.contact@gmail.com>
2015-06-18 11:32:44 -0700

    Add instructions to plotting tests.

--- test_plotting_interactive.py
+++ test_plotting_interactive.py
@@ -18,6 +18,13 @@ class TruismsTestCase(unittest.TestCase):
 class SegmentSelectorTestCase(unittest.TestCase):
 
     def test_correct_number_of_segments_stored(self):
+        instructions = ('\n'
+                        'Right click on the plot to highlight segments.\n'
+                        'Use the normal cursors to move/zoom as you do so.\n'
+                        'To remove a segment, right click somewhere in the middle of it.\n'
+                        'When you are satisfied, you can close the window.')
+        print(instructions)
+
         fig, axs = plt.subplots(2, 1, sharex=True)
 
         t = np.linspace(0, 100, 1001)
@@ -32,6 +39,12 @@ class SegmentSelectorTestCase(unittest.TestCase):
         plt.show()
 
     def test_load_with_set_of_segments(self):
+        instructions = ('\n'
+                       'This plot has been opened with some segments pre-loaded.\n'
+                       'Feel free to modify them in the same way as before.\n'
+                       'When you close this plot, the segments will be saved and they will be loaded automatically with the plot that follows.')
+        print(instructions)
+
         fig, axs = plt.subplots(2, 1, sharex=True)
 
         t = np.linspace(0, 100, 1001)
