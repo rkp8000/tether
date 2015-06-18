2ecbe2f12dd23b8c82da7346f89e9bd29aaf0ea0
rkp8000 <rpang.contact@gmail.com>
2015-06-18 12:27:39 -0700

    Remove test about indexes and all that.

--- test_plotting.py
+++ test_plotting.py
@@ -15,22 +15,5 @@ class TruismsTestCase(unittest.TestCase):
         self.assertTrue(True)
 
 
-class SegmentSelectorTestCase(unittest.TestCase):
-
-    def test_idx_from_time(self):
-        fig, axs = plt.subplots(3, 1, sharex=True)
-
-        t = np.linspace(0, 100, 1001)
-        segment_selector = plotting.SegmentSelector(fig, axs, time_vector=t)
-
-        self.assertEqual(segment_selector.idx_from_time(.3), 3)
-        self.assertEqual(segment_selector.idx_from_time(.29), 3)
-        self.assertEqual(segment_selector.idx_from_time(.24), 2)
-        self.assertEqual(segment_selector.idx_from_time(50.1), 501)
-        self.assertEqual(segment_selector.idx_from_time(50.14), 501)
-        self.assertEqual(segment_selector.idx_from_time(-1), 0)
-        self.assertEqual(segment_selector.idx_from_time(10000), 1000)
-
-
 if __name__ == '__main__':
     unittest.main()
