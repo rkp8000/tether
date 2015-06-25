1ff86a135d74687867dcfbbeddbf6b6fe5db10e1
rkp8000 <rpang.contact@gmail.com>
2015-06-25 15:00:26 -0700

    Include a random stimulus too, to examine the chances of finding an acausal correlation.

--- stripes_velocity_white_noise/view_cross_correlations_loop_through_trials.py
+++ stripes_velocity_white_noise/view_cross_correlations_loop_through_trials.py
@@ -18,8 +18,8 @@ from db_api.connect import session
 
 import edr_handling
 
-PLOT_TIME_SERIES = True
-PLOT_AUTO_CORRELATIONS = False
+PLOT_TIME_SERIES = False
+PLOT_AUTO_CORRELATIONS = True
 PLOT_P_VALUES = False
 
 FIG_SIZE_TIME_SERIES = (16, 16)
@@ -37,12 +37,17 @@ CONTROL_FILTER = np.concatenate([np.zeros(20,), CONTROL_FILTER[:-CONTROL_SHIFT]]
 CONTROL_NOISE = 0.000001
 N_TIMESTEPS_FILTER = len(CONTROL_FILTER)
 
+# for building a random time-series
+STIM_RANDOM_NOISE = 100
+
 LW = 2
 ALPHA = 0.3
 
 
 def main():
     for trial_id in TRIAL_IDS:
+
+        print('Trial {}'.format(trial_id))
         trial = session.query(models.Trial).get(trial_id)
 
         data, _, cols, _ = edr_handling.load_from_trial(trial, dt=DT, unwrap_barpos=True, lmr_zscore=False)
@@ -54,6 +59,7 @@ def main():
         lpr = [data_segment[:, cols.index('Lamp')] + data_segment[:, cols.index('Ramp')]
                for data_segment in data]
         freq = [data_segment[:, cols.index('Freq')] for data_segment in data]
+        stim_rand = [np.random.normal(0, STIM_RANDOM_NOISE, len(v)) for v in vel]
 
         # build a control response to make sure we'd be able to accurately recover the filter if there was one
         control = [sp_signal.fftconvolve(v, CONTROL_FILTER, 'full') for v in vel]
@@ -66,9 +72,13 @@ def main():
 
         vel_x_lmr, p_vel_x_lmr, lb_vel_x_lmr, ub_vel_x_lmr = \
             signal.xcov_simple_two_sided_multi(vel, lmr, n_lags_back=n_lags_back,
-                                               n_lags_forward=n_lags_forward, normed=True)
+                                               n_lags_forward=n_lags_forward, normed=True,
+                                               confidence=.95)
         vel_x_lpr, p_vel_x_lpr, lb_vel_x_lpr, ub_vel_x_lpr = \
             signal.xcov_simple_two_sided_multi(vel, lpr, n_lags_back=n_lags_back,
+                                               n_lags_forward=n_lags_forward, normed=True)
+        rand_x_lpr, p_rand_x_lpr, lb_rand_x_lpr, ub_rand_x_lpr = \
+            signal.xcov_simple_two_sided_multi(stim_rand, lpr, n_lags_back=n_lags_back,
                                                n_lags_forward=n_lags_forward, normed=True)
         vel_x_control, p_vel_x_control, lb_vel_x_control, ub_vel_x_control = \
             signal.xcov_simple_two_sided_multi(vel, control, n_lags_back=n_lags_back,
@@ -84,6 +94,9 @@ def main():
         vel_x_vel, p_vel_x_vel, lb_vel_x_vel, ub_vel_x_vel = \
             signal.xcov_simple_two_sided_multi(vel, vel, n_lags_back=n_lags_back,
                                                n_lags_forward=n_lags_forward, normed=True)
+        rand_x_rand, p_rand_x_rand, lb_rand_x_rand, ub_rand_x_rand = \
+            signal.xcov_simple_two_sided_multi(stim_rand, stim_rand, n_lags_back=n_lags_back,
+                                               n_lags_forward=n_lags_forward, normed=True)
         lmr_x_lmr, p_lmr_x_lmr, lb_lmr_x_lmr, ub_lmr_x_lmr = \
             signal.xcov_simple_two_sided_multi(lmr, lmr, n_lags_back=n_lags_back,
                                                n_lags_forward=n_lags_forward, normed=True)
@@ -97,7 +110,7 @@ def main():
         t = np.arange(-n_lags_back, n_lags_forward) * DT
 
         # plot cross-correlations
-        fig, axs = plt.subplots(4, 1, sharex=True, tight_layout=True)
+        fig, axs = plt.subplots(5, 1, sharex=True, tight_layout=True)
         [ax.axhline(0, ls='--') for ax in axs]
 
         axs[0].plot(t, vel_x_lmr, lw=LW)
@@ -106,16 +119,20 @@ def main():
         axs[1].plot(t, vel_x_lpr, lw=LW)
         axs[1].fill_between(t, lb_vel_x_lpr, ub_vel_x_lpr, color='b', alpha=ALPHA)
 
-        axs[2].plot(t, vel_x_freq, lw=LW)
-        axs[2].fill_between(t, lb_vel_x_freq, ub_vel_x_freq, color='b', alpha=ALPHA)
+        axs[2].plot(t, rand_x_lpr, lw=LW)
+        axs[2].fill_between(t, lb_rand_x_lpr, ub_rand_x_lpr, color='b', alpha=ALPHA)
 
-        axs[3].plot(t, vel_abs_x_freq, lw=LW)
-        axs[3].fill_between(t, lb_vel_abs_x_freq, ub_vel_abs_x_freq, color='b', alpha=ALPHA)
+        axs[3].plot(t, vel_x_freq, lw=LW)
+        axs[3].fill_between(t, lb_vel_x_freq, ub_vel_x_freq, color='b', alpha=ALPHA)
+
+        axs[4].plot(t, vel_abs_x_freq, lw=LW)
+        axs[4].fill_between(t, lb_vel_abs_x_freq, ub_vel_abs_x_freq, color='b', alpha=ALPHA)
 
         axs[0].set_ylabel('vel x lmr')
         axs[1].set_ylabel('vel x lpr')
-        axs[2].set_ylabel('vel x freq')
-        axs[3].set_ylabel('|vel| x freq')
+        axs[2].set_ylabel('rand x lpr')
+        axs[3].set_ylabel('vel x freq')
+        axs[4].set_ylabel('|vel| x freq')
 
         axs[3].set_xlabel('t (s)')
 
