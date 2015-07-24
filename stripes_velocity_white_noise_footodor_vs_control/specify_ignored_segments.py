476285986a94376d58762365e019b56e56bc3217
Rich Pang <rpang.contact@gmail.com>
2015-07-24 13:02:47 -0700

    Add code to specify ignored segments.

---
+++ stripes_velocity_white_noise_footodor_vs_control/specify_ignored_segments.py
@@ -0,0 +1,78 @@
+"""
+Go through a set of trials corresponding to this experiment and graphically specify what
+temporal segments to ignore.
+"""
+from __future__ import print_function, division
+
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+
+from db_api.connect import session
+from db_api import models
+
+import edr_handling
+from plotting import SegmentSelector, plot_trial_basic
+
+EXPERIMENT_ID = 'stripes_velocity_white_noise_footodor_vs_control'
+EARLIEST_DATETIME = datetime.datetime.strptime('2015-06-01', '%Y-%m-%d')
+
+# PLOT PARAMETERS
+FIG_SIZE = (12, 12)
+COLS = ('Freq', 'LmR', 'Barpos', 'Barvel')
+
+
+def main():
+    instructions = ('Right click on trial to mark starts and ends of segments.\n'
+                    'Close window to move on to next trial.')
+    print(instructions)
+
+    # get trials
+    trials = session.query(models.Trial). \
+        filter(models.Trial.experiment_id == EXPERIMENT_ID). \
+        filter(models.Trial.recording_start >= EARLIEST_DATETIME)
+
+    for trial in trials:
+        fig = plt.figure(facecolor='white', figsize=FIG_SIZE)
+        fig, axs, edr_data = plot_trial_basic(trial, fig, cols=COLS, dt=.02)
+
+        # define previously specified ignored segments if there are any
+        if trial.ignored_segments:
+            ignored_segments_simple = [(i_s.start_time, i_s.end_time) for i_s in trial.ignored_segments]
+        else:
+            ignored_segments_simple = None
+
+        t_min = edr_data[0, 0]
+        t_max = edr_data[-1, 0]
+
+        # create segment selector
+        segment_selector = SegmentSelector(fig, axs, t_min, t_max, segments=ignored_segments_simple)
+
+        plt.show(block=True)
+
+        # remove all ignored segments that were previously bound to this trial
+        [session.delete(i_s) for i_s in trial.ignored_segments]
+        trial.ignored_segments = []
+
+        # add all ignored segments that we've created in the segment selector
+        for segment in segment_selector.segments_simple:
+            ignored_segment = models.IgnoredSegment()
+            # convert time to time idx
+            ignored_segment.start_time = segment[0]
+            ignored_segment.end_time = segment[1]
+
+            trial.ignored_segments += [ignored_segment]
+
+    save = raw_input('Save [y or n]?')
+
+    if save.lower() == 'y':
+        # add all updated trials to database
+        for trial in trials:
+            session.add(trial)
+        session.commit()
+    else:
+        print('Data not saved.')
+        session.rollback()
+
+if __name__ == '__main__':
+    main()
